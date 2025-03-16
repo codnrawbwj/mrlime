5d94d346c503bf68094b93602bab99385351b903
codnrawbwj <codnrawbwj@gmail.com>
2025-03-16 17:38:28 -0500

    Fix minor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import LandingPage from "@/components/LandingPage";
 import TopNav from "@/components/TopNav";
 
 export default function Home() {
   return (
     <>
       <TopNav />
-      <main className="max-w-7xl mx-auto px-4"></main>
+      <main className="max-w-7xl mx-auto px-4">
+        <LandingPage />
+      </main>
     </>
   );
 }
--- src/components/TopNav/index.tsx
+++ src/components/TopNav/index.tsx
@@ -3,10 +3,10 @@ import React from "react";
 
 const TopNav = () => {
   return (
-    <nav className="h-14 sm:h-16 md:h-18 bg-mojito-cool-teal w-full shadow-md mojito-rum-white px-4 flex-center">
+    <nav className="h-20 bg-mojito-cool-teal w-full shadow-md mojito-rum-white px-4 flex-center">
       <div className="m-auto max-w-7xl w-full flex items-center justify-between px-4">
         {/* TODO: Put logo here instead */}
-        <p className="font-bold text-2xl sm:text-3xl">Mr.Lime</p>
+        <p className="font-bold text-2xl">Mr.Lime</p>
         {/* Mobile Menu */}
         <MobileHamburgerMenu />
       </div>
