ee9c9a558923da619fbd9da917c7212358254ee2
codnrawbwj <codnrawbwj@gmail.com>
2025-03-23 19:36:25 -0500

    Fix: relocate TopNav to top layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import TopNav from "@/components/TopNav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <TopNav />
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 import LandingPage from "@/components/LandingPage";
-import TopNav from "@/components/TopNav";
 
 export default function Home() {
   return (
-    <>
-      <TopNav />
-      <main className="max-w-7xl mx-auto px-4">
-        <LandingPage />
-      </main>
-    </>
+    <main className="max-w-7xl mx-auto px-4">
+      <LandingPage />
+    </main>
   );
 }
