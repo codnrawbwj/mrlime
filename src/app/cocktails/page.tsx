3f28a3d890c159c8de9e4ad7370bec6bf7245d5e
codnrawbwj <codnrawbwj@gmail.com>
2025-03-27 23:37:43 -0500

    Fix: adjust list-card's UI and refactor

--- src/app/cocktails/page.tsx
+++ src/app/cocktails/page.tsx
@@ -1,6 +1,5 @@
-import TasteLabel from "@/components/ui/taste-label";
+import ListCard from "@/components/ui/list-card";
 import { COCKTAIL_HASH, CocktailType } from "@/constant/dummy";
-import Image from "next/image";
 import React from "react";
 
 const Page = () => {
@@ -13,30 +12,9 @@ const Page = () => {
           autoFocus
         />
       </div>
-      <div className="flex-col justify-center items-center gap-5 w-[80vw]">
+      <div className="flex-col justify-center items-center gap-5">
         {COCKTAIL_HASH.map((cocktail: CocktailType) => (
-          <article
-            key={cocktail._id}
-            className="rounded-lg w-full aspect-[3/1] bg-mojito-white-rum mx-auto overflow-hidden shadow-md flex "
-          >
-            <div className="grow flex-col gap-2 justify-center px-4">
-              <h3 className="texl-3xl font-bold">{cocktail.name}</h3>
-              <div className="flex justify-start items-center gap-1">
-                {cocktail.tasteLabel.map((label) => (
-                  <TasteLabel key={label} tasteLabel={label} />
-                ))}
-              </div>
-              <p className="text-[12px] italic">
-                &quot;{cocktail.tasteReview}&quot;
-              </p>
-            </div>
-            <Image
-              src={cocktail.pictureURL}
-              width={120}
-              height={100}
-              alt={`${cocktail.name} card image`}
-            />
-          </article>
+          <ListCard key={cocktail._id} cocktail={cocktail} />
         ))}
       </div>
     </div>
---
+++ src/components/ui/list-card.tsx
@@ -0,0 +1,28 @@
+import TasteLabel from "@/components/ui/taste-label";
+import { CocktailType } from "@/constant/dummy";
+import Image from "next/image";
+import React from "react";
+
+const ListCard: React.FC<{ cocktail: CocktailType }> = ({ cocktail }) => {
+  return (
+    <article className="rounded-lg max-w-[340px] aspect-[3/1] bg-mojito-white-rum mx-auto overflow-hidden shadow-md flex ">
+      <div className="grow flex-col gap-2 justify-center px-4">
+        <h3 className="texl-3xl font-bold">{cocktail.name}</h3>
+        <div className="flex justify-start items-center gap-1">
+          {cocktail.tasteLabel.map((label) => (
+            <TasteLabel key={label} tasteLabel={label} />
+          ))}
+        </div>
+        <p className="text-[12px] italic">&quot;{cocktail.tasteReview}&quot;</p>
+      </div>
+      <Image
+        src={cocktail.pictureURL}
+        width={110}
+        height={100}
+        alt={`${cocktail.name} card image`}
+      />
+    </article>
+  );
+};
+
+export default ListCard;
