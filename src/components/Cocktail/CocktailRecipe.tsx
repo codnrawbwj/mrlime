6e9f126d054d8a6414fc7c3a7041c506e079c5a3
codnrawbwj <codnrawbwj@gmail.com>
2025-04-01 22:13:25 -0500

    Feat: render cocktail details in cocktail page

--- src/components/Cocktail/CocktailRecipe.tsx
+++ src/components/Cocktail/CocktailRecipe.tsx
@@ -7,13 +7,31 @@ interface CocktailRecipeProps {
 
 const CocktailRecipe: React.FC<CocktailRecipeProps> = ({ cocktail }) => {
   return (
-    <div className="w-full min-h-[calc(100vh-80px)] bg-mojito-rum-white flex-col items-center justify-start py-10 gap-15">
+    <div className="relative w-full min-h-[calc(100vh-80px)] bg-mojito-rum-white flex-col items-center justify-start py-10 gap-15">
       <ItemCard
         name={cocktail.name}
         baseSpirit={cocktail.ingredients[0]}
         img={cocktail.pictureURL}
         alcoholPercent={cocktail.alcoholPercent}
       />
+      {/* Taste Review */}
+      <div className="italic max-w-[320px] flex-center text-center">{`"${cocktail.tasteReview}"`}</div>
+      {/* Ingredient  */}
+      <div className="w-[340px] flex justify-start items-center rounded-2xl list-disc list-inside shadow-md px-4 py-6">
+        <ul>
+          {cocktail.ingredients.map((ing, index) => (
+            <li key={index}> - {ing}</li>
+          ))}
+        </ul>
+      </div>
+      {/* Recipe */}
+      <div className="w-[340px] flex-center rounded-2xl list-disc list-inside shadow-md px-4 py-6">
+        <ul>
+          {cocktail.recipe.map((step, index) => (
+            <li key={index}>{step}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
