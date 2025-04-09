c0e657258a77c42e4fd4846fe35d740579a45b1b
codnrawbwj <codnrawbwj@gmail.com>
2025-04-09 00:18:04 -0500

    Remove redundant schema field in Ingredients

--- server/src/models/Ingredient.ts
+++ server/src/models/Ingredient.ts
@@ -7,29 +7,9 @@ type IngredientType =
   | "garnish"
   | "carbonated"
   | "others";
-
-export const UnitValues = [
-  "ml",
-  "oz",
-  "tsp",
-  "tbsp",
-  "dash",
-  "drop",
-  "slice",
-  "piece",
-  "leaf",
-  "cube",
-  "whole",
-  "part",
-] as const;
-
-export type UnitValuesType = (typeof UnitValues)[number];
-
 export interface IIngredient extends Document {
   name: string;
   type: IngredientType;
-  abv: number;
-  unit: UnitValuesType[];
   image?: string;
   description: string;
   isMainAlcohol?: boolean;
@@ -38,8 +18,6 @@ export interface IIngredient extends Document {
 const IngredientSchema: Schema = new Schema({
   name: { type: String, required: true },
   type: { type: String, required: true },
-  abv: { type: Number, required: true },
-  unit: [{ type: String, required: true }],
   image: { type: String },
   description: { type: String },
   isMainAlcohol: { type: Boolean },
