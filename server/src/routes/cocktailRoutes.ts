1f6b2cbca572193d59192ffa4359c0c398d08c3c
codnrawbwj <codnrawbwj@gmail.com>
2025-04-12 17:16:19 -0500

    Restart server with Nest.js

--- server/src/routes/cocktailRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getAllCocktails } from "../controllers/cocktailController";
-
-const router = express.Router();
-
-router.get("/", getAllCocktails);
-
-export default router;
