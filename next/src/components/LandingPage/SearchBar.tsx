89c30379ad690ef2c5a101e5d55eaf2aa4f4a57d
codnrawbwj <codnrawbwj@gmail.com>
2025-04-24 23:00:11 -0500

    Feat: refactor SearchBar from LandingPage and add dropdown menu

---
+++ next/src/components/LandingPage/SearchBar.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { ArrowRightIcon } from "@/assets";
+import { COCKTAIL_HASH } from "@/constant/dummy";
+import { useState } from "react";
+
+const SearchBar = () => {
+  const [searchValue, setSearchValue] = useState<string | null>();
+
+  return (
+    <div className="w-[300px] md:w-[600px] shadow-md rounded-2xl overflow-hidden">
+      <div className="flex items-center justify-between pr-4">
+        <input
+          placeholder="Ask recipes"
+          className="text-[1rem] focus:outline-none truncate px-6 py-4"
+          // autoFocus
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+        <div className="w-8 h-8 rounded-xl border-1 flex-center">
+          <ArrowRightIcon style={{ width: 24, height: 24 }} />
+        </div>
+      </div>
+      {searchValue && (
+        <div className="mt-2">
+          {COCKTAIL_HASH.slice(0, 5).map((item) => (
+            <div
+              key={item._id}
+              className="py-2 flex-col hover:bg-[var(--mojito-green)] px-4"
+            >
+              {item.name}
+              <span className="mojito-text-b text-[0.625rem]">
+                {item.tasteReview}
+              </span>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SearchBar;
--- next/src/components/LandingPage/index.tsx
+++ next/src/components/LandingPage/index.tsx
@@ -1,20 +1,11 @@
-import { ArrowRightIcon } from "@/assets";
+import SearchBar from "@/components/LandingPage/SearchBar";
 import React from "react";
 
 const LandingPage = () => {
   return (
     <div className="w-full h-[calc(100vh-80px)] bg-mojito-rum-white flex-col items-center justify-start pt-20 gap-15">
       <h1 className="font-semibold text-2xl">🍹 What can I get for you?</h1>
-      <div className="w-[300px] rounded-2xl shadow-md flex items-center justify-between pr-4">
-        <input
-          placeholder="Ask recipes"
-          className="px-6 py-4  text-[1rem] focus:outline-none truncate"
-          autoFocus
-        />
-        <div className="w-8 h-8 rounded-xl border-1 flex-center">
-          <ArrowRightIcon style={{ width: 24, height: 24 }} />
-        </div>
-      </div>
+      <SearchBar />
     </div>
   );
 };
