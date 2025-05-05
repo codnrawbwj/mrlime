3232d9fa6af8c2559e92027b0ab556e711e7b99a
codnrawbwj <codnrawbwj@gmail.com>
2025-05-04 19:46:03 -0500

    Feat: show search related results on dropdown on search bar

--- next/src/components/LandingPage/SearchBar.tsx
+++ next/src/components/LandingPage/SearchBar.tsx
@@ -2,10 +2,31 @@
 import { ArrowRightIcon } from "@/assets";
 import { COCKTAIL_HASH } from "@/constant/dummy";
 import Link from "next/link";
-import { useState } from "react";
+import { useMemo, useState } from "react";
+
+type searchResultType = {
+  _id: string;
+  name: string;
+  tasteReview: string;
+};
 
 const SearchBar = () => {
   const [searchValue, setSearchValue] = useState<string | null>();
+  // MARK: TODO replace with API
+  const cocktailSearchResultList: searchResultType[] = COCKTAIL_HASH.map(
+    (item) => ({
+      _id: item._id,
+      name: item.name,
+      tasteReview: item.tasteReview,
+    })
+  );
+  const relativeSearchResult: searchResultType[] = useMemo(() => {
+    if (!searchValue) return [];
+
+    return cocktailSearchResultList.filter((item) => {
+      return item.name.toLowerCase().includes(searchValue.toLowerCase());
+    });
+  }, [searchValue, cocktailSearchResultList]);
 
   return (
     <div className="w-[300px] md:w-[600px] shadow-md rounded-2xl overflow-hidden">
@@ -22,7 +43,7 @@ const SearchBar = () => {
       </div>
       {searchValue && (
         <div className="mt-2">
-          {COCKTAIL_HASH.slice(0, 5).map((item) => (
+          {relativeSearchResult.slice(0, 5).map((item) => (
             <Link
               href={`/cocktails/${item.name}`}
               key={item._id}
