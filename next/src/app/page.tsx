543aaf8b9d6e257d8f0baa2cee7d41d6ca70f270
codnrawbwj <codnrawbwj@gmail.com>
2025-05-21 22:07:36 -0500

    feat: connect with server & refactor the structure

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,27 +1,9 @@
 import LandingPage from "@/components/LandingPage";
 
 export default async function Home() {
-  const revalidate = 86400;
-
-  try {
-    const response = await fetch("https://mr-lime.onrender.com/api/cocktails", {
-      next: { revalidate },
-    });
-
-    const cocktailList = await response.json();
-    console.log(cocktailList);
-
-    return (
-      <main className="max-w-7xl mx-auto px-4">
-        <LandingPage />
-      </main>
-    );
-  } catch (error) {
-    console.error("Failed to fetch cocktails:", error);
-    return (
-      <main className="max-w-7xl mx-auto px-4">
-        <LandingPage />
-      </main>
-    );
-  }
+  return (
+    <main className="max-w-7xl mx-auto px-4">
+      <LandingPage />
+    </main>
+  );
 }
